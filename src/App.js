2a229a8dafb367e703715bd4ea79583c674aef63
cqq <945327638@qq.com>
2021-06-27 23:14:10 +0800

    修改

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Header from "./components/Header";
+import List from "./components/List";
+import Footer from "./components/Footer";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        todos: [
+            {id: '001', name: '吃饭', done: true},
+            {id: '002', name: '睡觉', done: true},
+            {id: '003', name: '打豆豆', done: false},
+        ]
+    }
+    addTodo = (newTodo) => {
+        this.setState({
+           todos:[newTodo,...this.state.todos]
+        })
+    }
+
+    checkUpdate = (id) => {
+        // console.log(id);
+        const newTodos =[...this.state.todos].map((item) => {
+            if (item.id===id){item.done=!item.done;}
+            return item;
+        })
+        // console.log(newTodos);
+        this.setState({
+            todos:newTodos
+        })
+
+    }
+    deleteOneTodo = (id) => {
+        const newTodos =[...this.state.todos].filter((item) => {
+            return  item.id!==id;
+        })
+        // console.log(newTodos);
+        this.setState({
+            todos:newTodos
+        })
+    }
+    deleteAllDidTodo = () => {
+        const newTodos =[...this.state.todos].filter((item) => {
+            return  !item.done;
+        })
+        // console.log(newTodos);
+        this.setState({
+            todos:newTodos
+        })
+    }
+
+
+    selectAllOrSelectNone = (flag) => {
+        const newTodos =[...this.state.todos].map((item) => {
+             item.done=flag;
+             return item;
+        })
+        // console.log(newTodos);
+        this.setState({
+            todos:newTodos
+        })
+
+
+    }
+
+    render() {
+        return (
+
+                <div className="todo-container">
+                    <Header addTodo={this.addTodo}/>
+                    <List todos={this.state.todos} checkUpdate={this.checkUpdate} deleteOneTodo={this.deleteOneTodo}/>
+                    <Footer todos={this.state.todos} deleteAllDidTodo={this.deleteAllDidTodo} selectAllOrSelectNone={this.selectAllOrSelectNone}/>
+                </div>
+
+
+
+        );
+    }
 }
 
 export default App;
